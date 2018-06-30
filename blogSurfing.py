5dd9264e77c59b1985c61bf5a340c09f596df5d2
redtime2002@naver.com <redtime2002@naver.com>
2018-06-30 17:12:00 +0900

    great second step

--- blogSurfing.py
+++ blogSurfing.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from operator import eq
+from random import randint
 
 import time
 
@@ -16,6 +18,10 @@ receiverPw = "naver!23"
 senderPw = "naver!23"
 
 tmpTime = ""
+
+searchList = ["안개속의숙녀호", "나이아가라폭포", "혼블라워",
+              "프라임 타파웨어", "스마트 그릴", "월스트리트", "원적외선 쿡탑",
+              "마리나시티", "한국전쟁참전용사기념관"]
 
 driver = webdriver.Chrome("C:\selenium\chromedriver.exe")
 
@@ -60,9 +66,18 @@ def basicWriting():
 
     driver.switch_to_default_content()
 
+def focus_this_Window():
+    handles = driver.window_handles
+    driver.switch_to.window(handles[len(handles)-1])
+
+
 
 # 대화형 메일 만들기
 def makeConversation(num):
+    driver.get("http://mail.naver.com")
+
+    logIn(senderId, senderPw)
+
     driver.find_element_by_xpath("//*[@id='nav_snb']/div[1]/a[1]/strong").click()
     waitForIsElementPresent("//*[@id='toInput']")
 
@@ -117,67 +132,60 @@ def makeConversation(num):
 
 
 
-# 대화형 메일 만들기
-def makeConversation(num):
-    driver.find_element_by_xpath("//*[@id='nav_snb']/div[1]/a[1]/strong").click()
-    waitForIsElementPresent("//*[@id='toInput']")
+#   블로그 서핑
+def surfingBlog():
 
-    basicWriting()
-
-    driver.find_element_by_xpath("//*[@id='sendBtn']").click()
-
-    waitForIsElementPresent("//*[@id='sendresultDivContent']/div[2]/h4")
-    comment = driver.find_element_by_xpath("//*[@id='sendresultDivContent']/div[2]/h4").text
-
-    assert eq(comment, "메일을 성공적으로 보냈습니다.")
-
-    i = 0
-
-    while num > i:
-
-        logOut()
-        waitForIsElementPresent("//*[@id='container']/div/div[2]/div[1]/div/a/span")
-
-        if i % 2 == 0:
-            logIn(receiverId, receiverPw)
-        else:
-            logIn(senderId, senderPw)
-
-        driver.find_element_by_xpath("//*[@id='nav_snb']/div[3]/div/div[1]/ul/li[1]/span/a[1]").click()
-
-        waitForIsElementPresent("//*[@id='list_for_view']/ol/li[1]/div/div[2]")
-        driver.find_element_by_xpath("//*[@id='list_for_view']/ol/li[1]/div/div[2]").click()
-
-        waitForIsElementPresent("//*[@id='readBtnMenu']/div[1]/span[1]/button[1]")
-        driver.find_element_by_xpath("//*[@id='readBtnMenu']/div[1]/span[1]/button[1]").click()
-
-        time.sleep(1)
-        driver.find_element_by_xpath("//*[@id='sendBtn']").click()
-        waitForIsElementPresent("//*[@id='sendresultDivContent']/div[2]/h4")
-        comment = driver.find_element_by_xpath("//*[@id='sendresultDivContent']/div[2]/h4").text
-
-        if eq(comment, "메일을 보내지 못했습니다."):
-            driver.find_element_by_xpath("//*[@id='sendresultDivContent']/div[2]/p[2]/a").click()
-            waitForIsElementPresent("//*[@id='list_for_view']/ol/li[1]/div/div[2]/a/span/strong")
-            driver.find_element_by_xpath("//*[@id='list_for_view']/ol/li[1]/div/div[2]/a/span/strong").click()
-
-            time.sleep(1)
-
-            driver.find_element_by_xpath("//*[@id='sendBtn']").click()
-
-        elif eq(comment, "메일을 성공적으로 보냈습니다."):
-            assert eq(comment, "메일을 성공적으로 보냈습니다.")
-
-        i = i + 1
-        time.sleep(1)
+    for i in range(0, 100):
 
 
+        driver.get("http://www.naver.com")
+
+        rand = randint(0,len(searchList)-1)
+        driver.find_element_by_xpath("//input[@id='query']").send_keys(searchList[rand])
+        time.sleep(1 + randint(0, 2))
+
+        driver.find_element_by_xpath("//button[@id='search_btn']").send_keys(Keys.ENTER)
+        time.sleep(1 + randint(0, 2))
+
+        driver.find_element_by_xpath("//span[text()='블로그']").click()
+
+        for page in range (1,100):
+            cnt = 0
+            time.sleep(5 + randint(0, 3))
+
+            bloglist = driver.find_elements_by_xpath("//li[@class='sh_blog_top']")
+
+            for j in range(0, len(bloglist)):
+
+                print(bloglist[j].find_element_by_xpath("dl/dd/span/a[1]").text)
+                if eq(bloglist[j].find_element_by_xpath("dl/dd/span/a[1]").text, "dooboorang"):
+                    bloglist[j].find_element_by_xpath("dl/dt/a").click()
+                    cnt = 1
+
+                    focus_this_Window()
+
+                    time.sleep(10 + randint(0, 10))
+                    element = driver.find_element_by_xpath("//a[text()='인쇄']")
+                    element.location_once_scrolled_into_view
+
+                    time.sleep(10 + randint(0, 10))
+
+                    driver.close()
+
+                    focus_this_Window()
+
+                    break
+
+            # dooboorang 있었을 경우
+            if cnt == 1:
+                break
+            else:
+                driver.find_element_by_xpath("//a[text()='다음페이지']").click()
+                continue
+
+    time.sleep(10)
 
 
-driver.get("http://www.naver.com")
-
-logIn(senderId, senderPw)
-
-makeConversation(10)
+surfingBlog()
 
 driver.close()
